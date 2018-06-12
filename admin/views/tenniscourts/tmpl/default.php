c37866319dc6f5a3456ef74836d47871d86dfb05
Beke <beke.andras.mkolc@gmail.com>
2018-06-12 20:31:34 +0200

    step-by-step

--- admin/views/tenniscourts/tmpl/default.php
+++ admin/views/tenniscourts/tmpl/default.php
@@ -12,6 +12,7 @@ JHtml::_('formbehavior.chosen', 'select');
 
 $listOrder     = $this->escape($this->filter_order);
 $listDirn      = $this->escape($this->filter_order_Dir);
+
 ?>
 <form action="index.php?option=com_tenniscourt&view=tenniscourts" method="post" id="adminForm" name="adminForm">
 	<div class="row-fluid">
@@ -33,10 +34,10 @@ $listDirn      = $this->escape($this->filter_order_Dir);
 				<?php echo JHtml::_('grid.checkall'); ?>
 			</th>
 			<th width="90%">
-				<?php echo JHtml::_('grid.sort', 'COM_TENNISCOURT_TENNISCOURTS_NAME', 'greeting', $listDirn, $listOrder); ?>
+				<?php echo JHtml::_('grid.sort', 'COM_TENNISCOURT_TENNISCOURTS_NAME', 'name', $listDirn, $listOrder); ?>
 			</th>
 			<th width="5%">
-				<?php echo JHtml::_('grid.sort', 'COM_TENNISCOURT_PUBLISHED', 'published', $listDirn, $listOrder); ?>
+				<?php echo JHtml::_('grid.sort', 'COM_TENNISCOURT_PUBLISHED', 'open', $listDirn, $listOrder); ?>
 			</th>
 			<th width="2%">
 				<?php echo JHtml::_('grid.sort', 'COM_TENNISCOURT_ID', 'id', $listDirn, $listOrder); ?>
@@ -62,7 +63,7 @@ $listDirn      = $this->escape($this->filter_order_Dir);
 						</td>
 						<td>
 							<a href="<?php echo $link; ?>" title="<?php echo JText::_('COM_TENNISCOURT_EDIT_TENNISCOURT'); ?>">
-								<?php echo $row->greeting; ?>
+								<?php echo $row->name; ?>
 							</a>
 						</td>
 						<td align="center">
