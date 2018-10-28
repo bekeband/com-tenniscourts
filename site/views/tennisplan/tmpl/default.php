1df4a8d2fde273e871dffbdde67869ba972bb765
Beke <beke.andras.mkolc@gmail.com>
2018-10-28 06:16:59 +0100

    s

--- site/views/tennisplan/tmpl/default.php
+++ site/views/tennisplan/tmpl/default.php
@@ -9,14 +9,58 @@
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted Access');
+
+
+
+
 ?>
 <form action="index.php?option=com_tenniscourt&view=tennisplan" method="post" id="adminForm" name="adminForm">
 	<table frame="box" class="table table-striped table-hover">
 		<thead>
+		<?php 
+		
+		$model = $this->getModel();
+		
+		//		var_dump($model);
+		//		throw new Exception("Exception", 404);
+		$table = $model->getTable();
+		
+		$rows = $table->load();
+		
+		//		var_dump($this->id);
+		
+		JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_tenniscourt/tables');
+		
+		// Count of courts. TennisCourtTableTennisCourt
+		$courtsrow = JTable::getInstance('TennisCourt', 'TennisCourtTable', array());
+		
+		// Reserving database open. TennisCourtTableTennisReserve
+		$planning = JTable::getInstance('TennisReserve', 'TennisCourtTable', array());
+		
+		//        $median_date = JFactory::getDate();
+		$median_date = new DateTime('NOW');
+		$min_date = new DateTime('NOW');
+		$max_date = new DateTime('NOW');
+		
+		$minus_interval = new DateInterval('PT2H');
+		$plus_interval = new DateInterval('PT14H');
+		
+		$min_date->sub($minus_interval);
+		$max_date->add($plus_interval);
+		
+		$diff_hours = $max_date->diff($min_date);
+		
+		//        var_dump($min_date);
+		//        var_dump($max_date);
+		//        var_dump($diff_hours);
+		
+		$columns = ($diff_hours->d * 24) + $diff_hours->h;
+		
+		var_dump($columns);
+		?>
 		<tr>
-		<?php $courtsarr = array(1, 2, 3, 4);?>
-		<?php $hourarr = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11); foreach ($hourarr as $value) { ?>
-		    <th width="1%"><?php echo $value; ?></th>
+		<?php for ($hourvalue = 1; $hourvalue <= $columns; $hourvalue++) { ?>
+		    <th width="1%"><?php echo $hourvalue; ?></th>
 
 		<?php } ?>
 		
@@ -30,51 +74,13 @@ defined('_JEXEC') or die('Restricted Access');
 			</tr>
 		</tfoot>
 		<tbody>
-		<?php 
-		$model = $this->getModel();
 
-//		var_dump($model);
-//		throw new Exception("Exception", 404);
-		$table = $model->getTable();
-		
-		$rows = $table->load();
-		
-//		var_dump($this->id);
-		
-		JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_tenniscourt/tables');
-		
-		// Count of courts. TennisCourtTableTennisCourt
-        $courtsrow = JTable::getInstance('TennisCourt', 'TennisCourtTable', array());
-        
-        // Reserving database open. TennisCourtTableTennisReserve
-        $planning = JTable::getInstance('TennisReserve', 'TennisCourtTable', array());
-
-//        $median_date = JFactory::getDate();
-        $median_date = new DateTime('NOW');
-        $min_date = new DateTime('NOW');
-        $max_date = new DateTime('NOW');
-        
-        $minus_interval = new DateInterval('PT2H');    
-        $plus_interval = new DateInterval('P1D');
-        
-        $min_date->sub($minus_interval);
-        $max_date->add($plus_interval);
-        
-        $diff_hours = $max_date->diff($min_date);
-        
-//        var_dump($min_date);
-//        var_dump($max_date);
-//        var_dump($diff_hours);
-        
-        $columns = ($diff_hours->d * 24) + $diff_hours->h;
-        
-        var_dump($columns);
-        
-        
-        ?>
 			<?php foreach ($courtsarr as $value) { ?>
 			
 				<tr>
+					<td>
+					<?php echo $median_date; ?>
+					</td>
 					<td>
 					<?php for ($hourvalue = 1; $hourvalue <= $columns; $hourvalue++) { ?>
 		    			<th width="1%"><?php echo $hourvalue; ?></th>
